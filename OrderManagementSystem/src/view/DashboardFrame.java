719660de73f4759f64894939d3c470990b008985
Ali Mert Kaya <mert_k4433@hotmail.com>
2025-03-12 23:01:42 +0300

    Created DashboardFrame

--- OrderManagementSystem/src/view/DashboardFrame.java
+++ OrderManagementSystem/src/view/DashboardFrame.java
@@ -6,12 +6,21 @@ import entity.User;
 import javax.swing.*;
 
 public class DashboardFrame extends JFrame {
-    private JPanel mainPanel;
-    private JPanel topPanel;
-    private JLabel welcomeLabel;
+    private JPanel pnl_main;
+    private JLabel lbl_welcome;
     private JButton exitButton;
-    private JTabbedPane menuTab;
-    private JPanel customerPanel;
+    private JTabbedPane tab_menu;
+    private JPanel pnl_customer;
+    private JScrollPane scrl_customer;
+    private JTable tbl_customer;
+    private JTextField fld_f_customer_name;
+    private JComboBox cmb_customer_type;
+    private JButton btn_customer_filter;
+    private JButton btn_customer_filter_reset;
+    private JButton btn_customer_new;
+    private JLabel lbl_f_customer_name;
+    private JPanel pnl_customer_filter;
+    private JLabel lbl_f_customer_type;
     private User user;
 
     public DashboardFrame(User user) {
@@ -21,12 +30,12 @@ public class DashboardFrame extends JFrame {
             this.dispose();
         }
 
-        this.add(mainPanel);
+        this.add(pnl_main);
         this.setTitle("Müşteri Yönetim Sistemi");
         this.setSize(1000,500);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
-        this.welcomeLabel.setText("Hoşgeldiniz: " + this.user.getName());
+        this.lbl_welcome.setText("Hoşgeldiniz: " + this.user.getName());
 
         exitButton.addActionListener(e -> {
             this.dispose();
